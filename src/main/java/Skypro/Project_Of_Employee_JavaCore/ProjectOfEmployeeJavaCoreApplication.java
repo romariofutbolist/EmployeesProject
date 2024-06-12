f0c7d7de5b045913a8dc15df867f725a3f844122
romariofutbolist <66all@mail.ru>
2024-06-12 22:19:38 +0300

    Мастер ветка

---
+++ src/main/java/Skypro/Project_Of_Employee_JavaCore/ProjectOfEmployeeJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package Skypro.Project_Of_Employee_JavaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOfEmployeeJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOfEmployeeJavaCoreApplication.class, args);
+	}
+
+}
